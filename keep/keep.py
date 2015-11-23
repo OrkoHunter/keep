4037dc299f39816a36f16602882c72c0e03618aa
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-11-24 03:13:46 +0530

    list and show now show different things

--- keep/keep.py
+++ keep/keep.py
@@ -5,10 +5,33 @@ import sys
 def main():
     command = sys.argv[1:]
 
-    if command[0] in ['show', 'list']:
-        f = open("/home/" + getpass.getuser() + "/.keep", 'r')
+    if command[0] == 'show':
+        try:
+            f = open("/home/" + getpass.getuser() + "/.keep", 'r')
+        except IOError:
+            print("You have no saved commands")
+            sys.exit(2)
+        lineno = cmdno = 0
         for line in f.readlines():
-            print(line)
+            lineno += 1
+            if lineno % 2 != 0:
+                cmdno += 1
+                sys.stdout.write("-"*50 + "\n")
+                sys.stdout.write(str(cmdno) + ". " + line[2:])
+            else:
+                sys.stdout.write(line)
+        f.close()
+    elif command[0] == 'list':
+        try:
+            f = open("/home/" + getpass.getuser() + "/.keep", 'r')
+        except IOError:
+            print("You have no saved commands.")
+            sys.exit(2)
+        cmdno = 0
+        for line in f.readlines():
+            if (line[0]=='$'):
+                cmdno += 1
+                print(str(cmdno) + ". " + line[2:], end="")
         f.close()
     else:
         f = open("/home/" + getpass.getuser() + "/.keep", 'a')
