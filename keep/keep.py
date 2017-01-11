c6fd3034392faeb861a313710ecee9a94457969c
Dibya Prakash Das <dibyadascool@gmail.com>
2017-01-12 00:44:55 +0530

    Merged the previous two PRs.
    The grep option works and a command won't be added if it is already present.

--- keep/keep.py
+++ keep/keep.py
@@ -1,18 +1,21 @@
 import sys
 import os
 
+
 HOME = os.path.expanduser('~')
 KEEP_FILE = HOME + '/.keep'  # File to store commands
 KEEPN_FILE = HOME + '/.keep_info'  # File to store number of commands
 
-
+     
 def print_error_message():
     print("Usages :\n1. keep [options]")
     print("Options: list | reset ")
     print("2. keep <command_name> \n <brief_description_of_the_command>")
+    print("3. keep grep <search terms in the description>")
 
 def main():
     command = sys.argv[1:]
+    
     if not command:
         print_error_message()
         sys.exit(2)
@@ -25,7 +28,8 @@ def main():
         cmdno = 0
         for line in f.readlines():
             cmdno += 1
-            print(str(cmdno) + "." + line[1:], end="")
+            print(str(cmdno) + "." + line[:], end="")
+           
         f.close()
     elif command[0] == 'reset':
         prompt = input("This will erase all of your stored commands. "
@@ -41,14 +45,46 @@ def main():
                 pass
         else:
             print("Aborted.")
-    else:
+    elif command[0] == 'grep':
+        l=sys.argv[2:]
+        l=' '.join(l)
+        if not l:
+            print("No search terms")
+            sys.exit()
+        try:
+            f = open(KEEP_FILE,'r')
+            i = 0
+            for line in f.readlines():
+                s = (line.split(":")[1]).strip()
+                if not s.find(l.strip()) == -1:
+                    print(line)
+                    i += 1
+            if i == 0:
+                print("No matched terms")
+            f.close()
+        except IOError:
+            print("You have no saved commands")
+        
+
+    else 
         if not os.path.exists(KEEPN_FILE):
             f = open(KEEPN_FILE, 'w')
             f.write('0\n')
             f.close()
-
-        f = open(KEEP_FILE, 'a')
         new = ' '.join(command)
+        
+        try:
+            f = open(KEEP_FILE,'r')
+            for line in f.readlines():
+                if new.strip() == (line.split(":")[0])[2:].strip():
+                    print("Command already present")
+                    print(line)
+                    f.close()
+                    sys.exit()
+            f.close()
+        except IOError:
+            pass                
+        f = open(KEEP_FILE, 'a')
         desc = input("Description : ")
         f.write('$ ' + new + ' : ')
         f.write(desc + '\n')
@@ -73,4 +109,3 @@ def main():
 
     Total count of commands saved
 """
-
