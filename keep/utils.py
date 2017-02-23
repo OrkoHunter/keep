adda4d95c01355332349f374bbb12541f504fab3
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-02-23 17:01:27 +0530

    Style modifications for register

--- keep/utils.py
+++ keep/utils.py
@@ -9,7 +9,6 @@ import click
 import requests
 
 def save_command(cmd, desc):
-
     pass
 
 
@@ -18,30 +17,34 @@ def log(ctx, message):
     if ctx.verbose:
         ctx.log(message)
 
+
 def first_time_use(ctx):
-    click.secho("Hello ! Thank you for installing Keep", bold=True, fg='cyan')
     click.secho("Initializing environment in ~/.keep directory", fg='green')
     for i in range(2):
         click.echo('.', nl=False)
         time.sleep(0.5)
     click.echo('.OK', nl=True)
+
+    # Directory for Keep files
     dir_path = os.path.join(os.path.expanduser('~'), '.keep')
+
     os.mkdir(dir_path)
+
+    # User may not choose to register and work locally.
+    # Registration is required to push the commands to server
     if click.confirm('Proceed to register?', abort=True, default=True):
+        # Verify for existing user
         click.echo("Your credentials will be saved in the ~/.keep directory.")
         email = click.prompt('Email', confirmation_prompt=True)
-        json_res = {
-            'email': email
-        }
+        json_res = {'email': email}
         click.echo('Verifying with existing users...')
         r = requests.post('https://keep-cli.herokuapp.com/check-user', json=json_res)
         if r.json()['exists']:
             click.secho('User already exists !', fg='red')
             email = click.prompt('Email', confirmation_prompt=True)
-            json_res = {
-                'email': email
-            }
+            json_res = {'email': email}
             r = requests.post('https://keep-cli.herokuapp.com/check-user', json=json_res)
+        # Generate password for the user
         chars = string.ascii_letters + string.digits
         password = ''.join(random.choice(chars) for _ in range(255))
         credentials_file = os.path.join(dir_path, '.credentials')
@@ -49,10 +52,8 @@ def first_time_use(ctx):
             'email': email,
             'password': password
         }
-        with open(credentials_file, 'w+') as f:
-            f.write(json.dumps(credentials))
         click.secho("Generated password for " + email, fg='cyan')
-        click.secho(password, fg='cyan')
+        # Register over the server
         click.echo("Registering new user ...")
         json_res = {
             'email': email,
@@ -61,6 +62,10 @@ def first_time_use(ctx):
         r = requests.post('https://keep-cli.herokuapp.com/register', json=json_res)
         if r.status_code == 200:
             click.secho("User successfully registered !", fg='green')
-        click.secho("Credentials file saved at ~/.keep/.credentials", fg='green')
+            # Save the credentials into a file
+            with open(credentials_file, 'w+') as f:
+                f.write(json.dumps(credentials))
+            click.secho(password, fg='cyan')
+            click.secho("Credentials file saved at ~/.keep/.credentials", fg='green')
     sys.exit(0)
 
