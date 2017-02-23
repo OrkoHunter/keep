0cdb12150404686df51bfb8280a35c6c2062392c
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-02-23 18:31:13 +0530

    Added keep grep

--- keep/commands/cmd_grep.py
+++ keep/commands/cmd_grep.py
@@ -1,15 +1,35 @@
+
+import json
+import os
+import re
 import click
-from keep.cli import pass_context
+from keep import cli, utils
 
 
 @click.command('grep', short_help='Searches for a saved command.')
-@pass_context
-def cli(ctx):
-    """Saves a new command"""
-    cmd = click.prompt('Command : ')
-    desc = click.prompt('Description : ')
-    print(type(cmd))
-    print(type(desc))
-
-    if ctx.verbose:
-        ctx.log('Initialized the repository')
+@click.argument('pattern')
+@cli.pass_context
+def cli(ctx, pattern):
+    """Searches for a saved command."""
+    json_path = os.path.join(os.path.expanduser('~'), '.keep', 'commands.json')
+    if not os.path.exists(json_path):
+        click.echo('No commands to show. Add one by `keep new`.')
+    else:
+        FOUND = False
+        for cmd, desc in json.loads(open(json_path, 'r').read()).items():
+            if re.search(pattern, cmd + " :: " + desc):
+                FOUND = True
+                click.secho(cmd + " :: " + desc, fg='green')
+                break
+            # Show if all the parts of the pattern are in one command/desc
+            keywords_len = len(pattern.split())
+            i_keyword = 0
+            for keyword in pattern.split():
+                if keyword.lower() in cmd.lower() or keyword.lower() in desc.lower():
+                    FOUND = True
+                    i_keyword += 1
+            if i_keyword == keywords_len:
+                click.secho(cmd + " :: " + desc, fg='green')
+                break
+        if not FOUND:
+            click.echo('No saved commands matches the pattern "{}"'.format(pattern))
