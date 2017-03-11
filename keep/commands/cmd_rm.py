50398fc2fcc5b9d8f412d85627eabefc0d6b5280
yehuda g <yeudag@gmail.com>
2017-03-11 17:00:46 +0200

    Pep8 fixes

--- keep/commands/cmd_rm.py
+++ keep/commands/cmd_rm.py
@@ -12,7 +12,7 @@ def cli(ctx, pattern):
         click.echo("\n\n")
         for idx, match in enumerate(matches):
             cmd, desc = match
-            click.secho(" " + str(idx+1) + "\t", nl=False, fg='yellow')
+            click.secho(" " + str(idx + 1) + "\t", nl=False, fg='yellow')
             click.secho("$ {} :: {}".format(cmd, desc), fg='green')
         click.echo("\n\n")
 
@@ -20,11 +20,11 @@ def cli(ctx, pattern):
         while True and len(matches) > 1:
             val = click.prompt("Choose command to remove [1-{}] (0 to cancel)"
                                .format(len(matches)), type=int)
-            if val in range(len(matches)+1):
+            if val in range(len(matches) + 1):
                 break
             click.echo("Number is not in range")
         if val > 0:
-            cmd, desc = matches[val-1]
+            cmd, desc = matches[val - 1]
             command = "$ {} :: {}".format(cmd, desc)
             if click.confirm("Remove\n\t{}\n\n?".format(command), default=True):
                 commands = utils.read_commands()
--- keep/commands/cmd_run.py
+++ keep/commands/cmd_run.py
@@ -13,7 +13,7 @@ def cli(ctx, pattern):
         click.echo("\n\n")
         for idx, match in enumerate(matches):
             cmd, desc = match
-            click.secho(" " + str(idx+1) + "\t", nl=False, fg='yellow')
+            click.secho(" " + str(idx + 1) + "\t", nl=False, fg='yellow')
             click.secho("$ {} :: {}".format(cmd, desc), fg='green')
         click.echo("\n\n")
 
@@ -21,11 +21,11 @@ def cli(ctx, pattern):
         while True and len(matches) > 1:
             val = click.prompt("Choose command to execute [1-{}] (0 to cancel)"
                                .format(len(matches)), type=int)
-            if val in range(len(matches)+1):
+            if val in range(len(matches) + 1):
                 break
             click.echo("Number is not in range")
         if val > 0:
-            cmd, desc = matches[val-1]
+            cmd, desc = matches[val - 1]
             command = "$ {} :: {}".format(cmd, desc)
             if click.confirm("Execute\n\t{}\n\n?".format(command), default=True):
                 os.system(cmd)
