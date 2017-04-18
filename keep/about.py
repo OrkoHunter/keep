0fb95f6ffd337f03dbcabf6db0c08303137f9947
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-04-19 00:57:13 +0530

    Final release script

--- keep/about.py
+++ keep/about.py
@@ -1,2 +1,2 @@
 __name__ = 'keep'
-__version__ = '2.4.1'
+__version__ = '2.4.2'
---
+++ release.sh
@@ -0,0 +1,3 @@
+python setup.py bdist_wheel upload
+python3 setup.py bdist_wheel upload
+python setup.py sdist upload
