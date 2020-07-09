165e1d525f56bcddadb484e18550ceb9f51c5ee5
Himanshu Mishra <himanshu@orkohunter.net>
2020-07-09 23:07:46 +0900

    Improve messages printed to the user

--- keep/commands/cmd_github_token.py
+++ keep/commands/cmd_github_token.py
@@ -20,9 +20,10 @@ def cli(ctx):
                          abort=True):
             os.remove(dir_path)
 
-    token = click.prompt('Create a GitHub token with permission to Gists and paste here ')
+    token = click.prompt('Create a GITHUB TOKEN with permission to Gists and paste here ')
     gist_id = existing_token.get('gist') if existing_token else None
-    prompt_str = 'If you already have a GitHub gist created by keep, paste the Gist ID here, or else press Enter '
+    click.echo("\nAwesome! Your GitHub token has been registered. Now, we need a GIST ID to store the commands in keep.")
+    prompt_str = 'If you already have a GitHub Gist created by keep, paste the GIST ID here, or else press Enter (we will create a Gist for you). '
     new_gist_id = click.prompt(prompt_str, default='', show_default=False)
 
     if new_gist_id:
@@ -39,4 +40,5 @@ def cli(ctx):
         json.dump({'token': token, 'gist': gist_id}, f)
 
     click.echo("Done! Gist URL - https://gist.github.com/" + gist_id)
+    click.echo("Your token and gist ID has been stored inside ~/.keep/.credentials")
     click.echo("You can now use `keep push` or `keep pull`")
