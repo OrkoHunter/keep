f70521b4e5f268b238f71740378c011c5738a9d9
Himanshu Mishra <himanshu@orkohunter.net>
2024-08-01 01:30:35 +0530

    new release

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
-# NOTE: Make sure to update version number
-# pip install twine wheel
+# NOTE: Make sure to update version number in pyproject.toml
+
+# Install reps needed
+# pip install twine wheel build
 rm -rf build/*
 rm -rf dist/*
 python -m build
